95b5d141eade11896530c2c0ccdc4e47ae645e5b
Vladimir <vladimir.rdguez@gmail.com>
2023-12-08 11:36:47 -0500

    minor fixings

--- build/internal/immutable/chunks/paths.4423bd80.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_qstzg2)==null?void 0:s.base)??"";var t;const a=((t=globalThis.__sveltekit_qstzg2)==null?void 0:t.assets)??"https://www.ladetec.com/pdl";export{a,e as b};
