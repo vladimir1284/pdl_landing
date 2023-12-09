ef213aee60e0f21a06c50bb38432e6339edebeb4
vladimir1284 <vladimir.rdguez@gmail.com>
2023-12-08 22:01:24 -0500

    gallery path fixing

--- build/internal/immutable/chunks/paths.1a4f44b4.js
+++
@@ -1,1 +0,0 @@
-var s;const i=((s=globalThis.__sveltekit_irtiii)==null?void 0:s.base)??"";var t;const e=((t=globalThis.__sveltekit_irtiii)==null?void 0:t.assets)??"https://www.ladetec.com/pdl";export{e as a,i as b};
